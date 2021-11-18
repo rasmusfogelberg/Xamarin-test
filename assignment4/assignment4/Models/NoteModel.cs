c0c924c2e84b60c992f9c316c6f1a55dbdc34f1e
Almyst <34063799+Almyst@users.noreply.github.com>
2021-11-18 17:58:31 +0100

    Add project files.

---
+++ assignment4/assignment4/Models/NoteModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment4
+{
+    public class NoteModel
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ assignment4/assignment4/Views/DetailPage.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+using ConstConfig;
+
+namespace assignment4
+{
+    public class DetailPage : ContentPage
+    {
+        public DetailPage(DetailPageViewModel viewModel)
+        {
+            BindingContext = viewModel;
+
+            Title = "Note Details";
+
+            BackgroundColor = Color.FromHex(Const.PrimaryColor);
+
+            var textLabel = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.FromHex(Const.TextColor),
+                FontSize = 16
+            };
+            textLabel.SetBinding(Label.TextProperty, nameof(DetailPageViewModel.NoteText));
+
+            var exitButton = new Button
+            {
+                Text = "Go Back",
+                VerticalOptions = LayoutOptions.Center,
+                Margin = new Thickness(20),
+                BackgroundColor = Color.FromHex(Const.ButtonColor),
+                FontSize = 20
+            };
+            exitButton.SetBinding(Button.CommandProperty, nameof(DetailPageViewModel.ExitCommand));
+
+            var stackLayout = new StackLayout
+            {
+                Margin = new Thickness(20, 40)
+            };
+            stackLayout.Children.Add(textLabel);
+            stackLayout.Children.Add(exitButton);
+
+            Content = stackLayout;
+        }
+    }
+}
